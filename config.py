e03f868f4f8a556b9c8bf84e1fc219a42b7da829
Neha Agrawal <88786146+iamproaf@users.noreply.github.com>
2024-04-06 16:27:36 +0530

    Update config.py

--- config.py
+++ config.py
@@ -83,13 +83,13 @@ START_IMG_URL = getenv(
 PING_IMG_URL = getenv(
     "PING_IMG_URL", "https://telegra.ph/file/e866a45ba88ffafd08918.jpg"
 )
-PLAYLIST_IMG_URL = "https://telegra.ph/file/e866a45ba88ffafd08918.jpg"
+PLAYLIST_IMG_URL = "https://telegra.ph/file/8544e55502ccc7e7a835d.jpg"
 STATS_IMG_URL = "https://telegra.ph/file/cc5b7bb650156ad0a8248.jpg"
 TELEGRAM_AUDIO_URL = "https://telegra.ph/file/e866a45ba88ffafd08918.jpg"
 TELEGRAM_VIDEO_URL = "https://telegra.ph/file/e866a45ba88ffafd08918.jpg"
 STREAM_IMG_URL = "https://te.legra.ph/file/bd995b032b6bd263e2cc9.jpg"
 SOUNCLOUD_IMG_URL = "https://te.legra.ph/file/bb0ff85f2dd44070ea519.jpg"
-YOUTUBE_IMG_URL = "https://te.legra.ph/file/6298d377ad3eb46711644.jpg"
+YOUTUBE_IMG_URL = "https://telegra.ph/file/8544e55502ccc7e7a835d.jpg"
 SPOTIFY_ARTIST_IMG_URL = "https://te.legra.ph/file/37d163a2f75e0d3b403d6.jpg"
 SPOTIFY_ALBUM_IMG_URL = "https://te.legra.ph/file/b35fd1dfca73b950b1b05.jpg"
 SPOTIFY_PLAYLIST_IMG_URL = "https://te.legra.ph/file/95b3ca7993bbfaf993dcb.jpg"

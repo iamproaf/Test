0a0bf2c24adb809ef9f922e4107a52fadab9f5e6
Neha Agrawal <88786146+iamproaf@users.noreply.github.com>
2024-05-01 17:01:39 +0530

    Update config.py

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/iamproaf/Test",
+    "https://github.com/iamproaf/Testi",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
